141826677feee97a68ab92aded085307d11b8262
jordi-gh <jorditorreszapata@gmail.com>
2016-08-19 20:46:47 +0200

    Cambios login @Raul

--- R/ui.R
+++ R/ui.R
@@ -1,105 +1,17 @@
-########################################################################
-# Script Karajaulas UI
-#install.packages("devtools")
-#devtools::install_github("rstudio/shiny-incubator")
-# css bootstrap: http://bootswatch.com/flatly/
-########################################################################
-require(Biobase)
-library(shiny)
-library(shinyBS)
-library(GEOquery)
-library(GEOmetadb)
-library(couchDB)
-library(DT)
-library(RSQLite)
+library(shinyjs)
 
-shinyUI
-(     fluidPage( 
+shinyUI(fluidPage(
   
   theme = "bootstrap.css",
   includeCSS("www/omics.css"),
-  includeScript("www/waiting.js"),
+  includeCSS("www/style.css"),
+  includeScript("www/md5.js"),
+  includeScript("www/passwdInputBinding.js"),
   
-  navbarPage(   
-    title="Omics-in-Cloud",
-    
-    windowTitle="Omics-in-Cloud",
-    
-    collapsible=TRUE,
-    
-    # Home
-    tabPanel("Home", id = "home",
-             icon = icon("fa fa-cloud"),
-             mainPanel(
-               h4("Página principal")
-             )
-    ),
-    
-    # Catalog
-    tabPanel("Data Catalog", icon = icon("fa fa-th"),
-             navlistPanel(
-               "Data Catalog", widths = c(2, 10),
-               tabPanel("ICO data", id = "icodata"),
-               
-               tabPanel("Other data", id = "otherdata",
-                        sidebarLayout(
-                          sidebarPanel(
-                            textInput("searchexperiment", label = h3("Search"), value = "Enter text..."),
-                            # select dataset
-                            uiOutput("choose_dataset"),
-                            # select platform
-                            uiOutput("choose_platform")
-                          ),
-                          
-                          mainPanel(
-                            textOutput("salidaprueba")
-                          )
-                        )
-               )
-             )
-    ),
-    
-    # Load Data
-    tabPanel("Load Data", icon = icon("fa fa-database"),
-             navlistPanel(
-               "Load Data", widths = c(2, 10),
-               tabPanel("NCBI", icon = icon("fa fa-cloud-download"), id = "ncbi",
-                        sidebarLayout(
-                          sidebarPanel(textInput("experimentupload", label = h3("Experiment name"), value = "Enter text..."),
-                                       actionButton("submitexperiment", "Download experiment")),
-                          mainPanel(
-                            tags$div(class = "waiting", p("Progress.."), img(src="img/loader.gif")),
-                            dataTableOutput("experiment")
-                          )
-                        )
-               ),
-               tabPanel("ICO", icon = icon("fa fa-cloud-upload"), id = "ico",
-                        sidebarLayout(
-                          sidebarPanel(fileInput('file1', h3('Choose file to upload'))),
-                          mainPanel(
-                            tags$div(class = "waiting", p("Progress.."), img(src="img/loader.gif")),
-                            dataTableOutput('contents')
-                          )
-                        )
-               )
-             )
-    ),
-    
-    # Análisis
-    tabPanel("Analysis", icon = icon("fa fa-bar-chart"),
-             navlistPanel(
-               "Analysis", widths = c(2, 10),
-               tabPanel("Anal1", id = "anal1"),
-               tabPanel("Anal2", id = "anal2")
-             )
-    ),
-    
-    # Ayuda
-    tabPanel("Help", 
-             icon = icon("fa fa-question"), id = "help"
-    )
-    
-  )
-) # final fluidPage
-)
+  useShinyjs(),
+  
+  fluidRow(align="center", h1("Omics-in-Cloud  ",img(src = "img/logoICO.png"))),
 
+  uiOutput("app")
+  
+))
