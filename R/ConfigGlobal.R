12647e8cbf1223c4180bba931c17beef5af23a97
jordi-gh <jorditorreszapata@gmail.com>
2016-08-26 21:39:49 +0200

    Script per variables globals comunes

---
+++ R/ConfigGlobal.R
@@ -0,0 +1,6 @@
+## Variables globals comunes, cal comitejar aquest fitxer
+
+# Connexió a bd sqlite: metadata
+assign("db_meta", NULL, envir = .GlobalEnv)
+# Connexió a bd SQLite: GEOMetadb
+assign("db_geo", NULL, envir = .GlobalEnv)
