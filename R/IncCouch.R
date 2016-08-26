5824c613924f0ebbcf38f6582b326ac25ecdb462
jordi-gh <jorditorreszapata@gmail.com>
2016-08-26 11:36:49 +0200

    Nou format dels ExpressionSet i GSe a CouchDB

--- R/IncCouch.R
+++ R/IncCouch.R
@@ -5,20 +5,35 @@ library(RCurl)
 library(R4CouchDB)
 
 #########################################################################
-#Cal tenir instal·lat i actiu couchdb i una bd creada amb el nom 'geodb'
+#Cal tenir instal?lat i actiu couchdb i una bd creada amb el nom 'geodb'
 #########################################################################
 
 GeoACouch <- function(objGEO,nom) {
-  #Demanem un UUID a CouchDB. Això ho podriem fer nosaltres amb llibraria R uuid
-  #Caldrà veure si és una millor opció
+  #Demanem un UUID a CouchDB. Ho podriem fer nosaltres amb llibraria R uuid
+  #Caldra veure si es una millor opcio
   response <- fromJSON(getURL("http://localhost:5984/_uuids"))
   uid=response$uuid
-  #Convertim taula de dades a JSON, si en té
-  taula<-toJSON(Table(objGEO))
-  #Convertim metadades a JSON
-  metadades<-toJSON(Meta(objGEO))
-  #Preparem registre per inserir a BD
-  newreg<-toJSON(list(nom=nom,metadades=metadades,taula=taula))
+  #Convertim taula de dades a JSON, si en te
+  if (toupper(class(objGEO))=='EXPRESSIONSET'){
+    eset=toJSON(objGEO)
+    newreg<-toJSON(list(nom=nom,expression_set=eset))
+  } else if (toupper(class(objGEO))=='GSE'){
+    #Convertim metadades a JSON
+    metadades<-toJSON(Meta(objGEO))
+    #Convertim llista de GPLs a JSON
+    gpls<-toJSON(objGEO@gpls)
+    #Convertim llista de GSMs a JSON
+    gsms<-toJSON(objGEO@gsms)
+    #Preparem registre per inserir a BD
+    newreg<-toJSON(list(nom=nom,metadades=metadades,gpls=gpls,gsms=gsms))
+  }
+  else {
+    taula<-toJSON(Table(objGEO))
+    #Convertim metadades a JSON
+    metadades<-toJSON(Meta(objGEO))
+    #Preparem registre per inserir a BD
+    newreg<-toJSON(list(nom=nom,metadades=metadades,taula=taula))
+  }
   getURL(paste("http://localhost:5984/geodb/",uid,sep=""),
          customrequest="PUT",
          httpheader=c('Content-Type'='application/json'),
