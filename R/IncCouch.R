9d2ecc647aff2dfac574b5c29d173a7be29589ac
jordi-gh <jorditorreszapata@gmail.com>
2016-09-13 23:56:02 +0200

    Nova funció CouchAGEO Modificada funció GEOACouch (original ara és GEOACouch_old)

--- R/IncCouch.R
+++ R/IncCouch.R
@@ -8,16 +8,17 @@ library(R4CouchDB)
 #Cal tenir instal?lat i actiu couchdb i una bd creada amb el nom 'geodb'
 #########################################################################
 
-GeoACouch <- function(objGEO,nom) {
+GeoACouch_old <- function(objGEO,nom) {
   #Demanem un UUID a CouchDB. Ho podriem fer nosaltres amb llibraria R uuid
   #Caldra veure si es una millor opcio
   response <- fromJSON(getURL("http://localhost:5984/_uuids"))
   uid=response$uuid
   #Convertim taula de dades a JSON, si en te
-  if (toupper(class(objGEO))=='EXPRESSIONSET'){
+  tipusGEO <- toupper(class(objGEO))
+  if (tipusGEO =='EXPRESSIONSET'){
     eset=toJSON(objGEO)
-    newreg<-toJSON(list(nom=nom,expression_set=eset))
-  } else if (toupper(class(objGEO))=='GSE'){
+    newreg<-toJSON(list(nom=nom,tipus=tipusGEO,expression_set=eset))
+  } else if (tipusGEO=='GSE'){
     #Convertim metadades a JSON
     metadades<-toJSON(Meta(objGEO))
     #Convertim llista de GPLs a JSON
@@ -25,15 +26,32 @@ GeoACouch <- function(objGEO,nom) {
     #Convertim llista de GSMs a JSON
     gsms<-toJSON(objGEO@gsms)
     #Preparem registre per inserir a BD
-    newreg<-toJSON(list(nom=nom,metadades=metadades,gpls=gpls,gsms=gsms))
+    newreg<-toJSON(list(nom=nom,tipus=tipusGEO,metadades=metadades,gpls=gpls,gsms=gsms))
   }
   else {
     taula<-toJSON(Table(objGEO))
     #Convertim metadades a JSON
     metadades<-toJSON(Meta(objGEO))
     #Preparem registre per inserir a BD
-    newreg<-toJSON(list(nom=nom,metadades=metadades,taula=taula))
+    newreg<-toJSON(list(nom=nom,tipus=tipusGEO,metadades=metadades,taula=taula))
   }
+  getURL(paste("http://localhost:5984/geodb/",uid,sep=""),
+         customrequest="PUT",
+         httpheader=c('Content-Type'='application/json'),
+         postfields=newreg)
+  #Retornem uid per guardar-lo a model relacional
+  return (uid)
+}
+
+GeoACouch <- function(objGEO,nom) {
+  #Demanem un UUID a CouchDB. Ho podriem fer nosaltres amb llibraria R uuid
+  #Caldra veure si es una millor opcio
+  response <- fromJSON(getURL("http://localhost:5984/_uuids"))
+  uid=response$uuid
+  #Convertim objecte a JSON
+  classeGEO <- class(objGEO)
+  jsonGEO=toJSON(objGEO)
+  newreg<-toJSON(list(nom=nom,classe=classeGEO,objecte=jsonGEO))
   getURL(paste("http://localhost:5984/geodb/",uid,sep=""),
          customrequest="PUT",
          httpheader=c('Content-Type'='application/json'),
@@ -70,6 +88,32 @@ CouchAICO <- function(uid){
   return(strJSON)
 }
 
+
+#Llegir un fitxer GEO de CouchDB
+CouchAGEO <- function(uid){
+  response <- getURL(paste("http://localhost:5984/geodb/",uid,sep=""))
+  #Retornem el document en format JSON
+  llistareg <- fromJSON(response)
+  #Si no l'hem trobat sortim
+  if (is.null(llistareg)){
+    return(FALSE)
+  }
+  #Si no és GEO sortim
+  if (is.null(llistareg['classe'])){
+    return (FALSE)
+  }
+  classeGEO<-llistareg['classe']
+
+  #Control classe vàlida
+  if(!is.element(classeGEO,c('ExpressionSet','GPL','GSM','GDS','GSE'))){
+    return(FALSE) #No és tipus GEO reconegut
+  }  
+  objGEO=new(classeGEO)
+  objGEO<-fromJSON(llistareg['objecte'])
+  return(objGEO)
+}
+
+
 #Eliminar document de couch per uid
 eliminaCouch <- function(uid=NULL,rev=NULL){
   #Si no tenim uid definit no fem res: PODRIEM ELIMINAR DB SENCERA
@@ -80,8 +124,9 @@ eliminaCouch <- function(uid=NULL,rev=NULL){
   #Per eliminar hem d'afegir la revisió del document
   url<-paste(url,rev,sep='?rev=')
   response <- getURL(url,
-         customrequest="DELETE",
-         httpheader=c('Content-Type'='application/json'))
+                     customrequest="DELETE",
+                     httpheader=c('Content-Type'='application/json'))
   return(fromJSON(response))
 }
+
 
